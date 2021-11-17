8f915963995a691945b12d8052b74be7ff114bfe
NovaObrien11 <93271904+NovaObrien11@users.noreply.github.com>
2021-11-17 11:49:43 -0700

    Initial commit

---
+++ Areas/HelpPage/Models/TalkModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheCodeCamp.Areas.HelpPage.Models
+{
+    public class TalkModel
+    {
+        [Required]
+        public int TalkId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(4096, MinimumLength = 50)]
+        public string Abstract { get; set; }
+        [Range(100,500)]
+        [Required]
+        public int Level { get; set; }
+        public SpeakerModel Speaker { get; set; }
+    }
+}
---
+++ Controllers/TalksController.cs
@@ -0,0 +1,153 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using TheCodeCamp.Areas.HelpPage.Models;
+using TheCodeCamp.Data;
+
+namespace TheCodeCamp.Controllers
+{
+        [RoutePrefix("api/camps/{moniker}/talks")]
+    public class TalksController : ApiController
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public TalksController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }   
+        [Route()]
+        public async Task<IHttpActionResult> Get(string moniker, bool includeSpeakers = false)
+        {
+            try
+            {
+                var results = await _repository.GetTalksByMonikerAsync(moniker, includeSpeakers);
+
+                return Ok(_mapper.Map<IEnumerable<TalkModel>>(results));
+            }
+            catch (Exception ex)
+            {  
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{talkId:int}", Name = "GetTalk")]
+        public async Task<IHttpActionResult> Get(string moniker, int talkId, bool includeSpeakers = false)
+        {
+            try
+            {
+                var result = await _repository.GetTalkByMonikerAsync(moniker, talkId, includeSpeakers);
+                if (result == null) return NotFound();
+
+                return Ok(_mapper.Map<TalkModel>(result));
+
+
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route()]
+        public async Task<IHttpActionResult> Post(string moniker, TalkModel model)
+        {
+            try
+            {
+                
+                if (ModelState.IsValid)
+                {
+                    var camp = await _repository.GetCampAsync(moniker);
+                    if(camp != null)
+                    {
+                        var talk = _mapper.Map<Talk>(model);
+                        talk.Camp = camp;
+
+                        //Model the speaker if necessary
+                        if(model.Speaker != null)
+                        {
+                            var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                        }
+
+                        _repository.AddTalk(talk);
+
+                        if (await _repository.SaveChangesAsync())
+                        {
+                            return CreatedAtRoute("GetTalk", new { moniker = moniker, talkId = talk.TalkId }, _mapper.Map<TalkModel>(talk));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return BadRequest(ModelState);
+
+        }
+        [Route("{talkId:int}")]
+        public async Task<IHttpActionResult> Put(string moniker, int talkId, TalkModel model)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var talk = await _repository.GetTalkByMonikerAsync(moniker, talkId, true);
+                    if (talk == null) return NotFound();
+
+                    //should ignore speaker
+                    _mapper.Map(model, talk);
+
+                    //change speaker if needed
+
+                    if(talk.Speaker.SpeakerId != model.Speaker.SpeakerId || talk.Speaker == null)
+                    {
+                        var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                        if (speaker != null) talk.Speaker = speaker;
+                    }
+
+                    if(await _repository.SaveChangesAsync())
+                    {
+                        return Ok(_mapper.Map<TalkModel>(talk));
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [Route("{talkId:int}")]
+        public async Task<IHttpActionResult> Delete(string moniker, int talkId)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, talkId);
+                if (talk == null) return NotFound();
+
+                _repository.DeleteTalk(talk);
+
+                if(await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return InternalServerError();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
